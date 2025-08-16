97ad47b16f183d398bc19c5493d528c6d07247dd
Alan Huynh <hdmquan@outlook.com>
2025-08-16 14:50:12 +1000

    chore(global): initialize project from my personal website code

---
+++ .eleventy.js
@@ -0,0 +1,84 @@
+// Imports
+const pluginEleventyNavigation = require("@11ty/eleventy-navigation");
+const pluginMinifier = require("@sherby/eleventy-plugin-files-minifier");
+const pluginSitemap = require("@quasibit/eleventy-plugin-sitemap");
+
+const dateFilter = require("nunjucks-date-filter");
+
+// Configs
+const configCss = require("./src/config/css");
+const configJs = require("./src/config/javascript");
+const configSitemap = require("./src/config/sitemap");
+const configServer = require("./src/config/server");
+
+// Other
+const filterPostDate = require("./src/config/postDate");
+const isProduction = configServer.isProduction;
+
+module.exports = function (eleventyConfig) {
+    const startYear = 2022;
+    const currentYear = new Date().getFullYear();
+
+    eleventyConfig.addGlobalData("startYear", startYear);
+    eleventyConfig.addGlobalData("currentYear", currentYear);
+
+    // Add Template Formats
+    eleventyConfig.addTemplateFormats("css");
+    eleventyConfig.addExtension("css", configCss);
+
+    eleventyConfig.addTemplateFormats("js");
+    eleventyConfig.addExtension("js", configJs);
+
+    // Add Plugins
+    eleventyConfig.addPlugin(pluginEleventyNavigation);
+    eleventyConfig.addPlugin(pluginSitemap, configSitemap);
+    eleventyConfig.addNunjucksFilter("date", dateFilter);
+
+    if (isProduction) {
+        eleventyConfig.addPlugin(pluginMinifier);
+    }
+
+    // Passthrough Copy
+    eleventyConfig.addPassthroughCopy("./src/assets");
+    eleventyConfig.addPassthroughCopy("./src/admin");
+    eleventyConfig.addPassthroughCopy("./src/_redirects");
+
+    // Add Filters
+    eleventyConfig.addFilter("postDate", filterPostDate);
+
+    // Add Shortcodes
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+    // Add Custom Collections
+    eleventyConfig.addCollection("blog", (collectionApi) => {
+        return collectionApi
+            .getFilteredByGlob("src/content/blog/*.md")
+            .reverse(); // Reverse for newest first
+    });
+
+    eleventyConfig.addCollection("project", (collectionApi) => {
+        return collectionApi
+            .getFilteredByGlob("src/content/project/*.md")
+            .reverse(); // Reverse for newest first
+    });
+
+    eleventyConfig.addCollection("songs", (collectionApi) => {
+        return collectionApi
+            .getFilteredByGlob("src/content/songs/*.md")
+            .reverse(); // Reverse for newest first
+    });
+
+    // Set Server Options
+    eleventyConfig.setServerOptions(configServer);
+
+    // Return Configuration
+    return {
+        dir: {
+            input: "src",
+            output: "public",
+            includes: "_includes",
+            data: "_data",
+        },
+        htmlTemplateEngine: "njk", // Use Nunjucks for HTML templates
+    };
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# My personal website
+
+Tech stack: 11ty + Nunjucks, SCSS, JS  
+Host on Netlify
+
+## Landing Preview
+
+![Landing light](assets/landing-light.jpeg "Landing light")
+![Landing dark](assets/landing-dark.jpeg "Landing dark")
